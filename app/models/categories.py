207c22fa400fb1deedf5b50db5036a38da9bbaf7
mikhail-sereda <mikhail.sereda.dev@yandex.ru>
2026-03-29 22:38:40 +0900

    feat: add models, categories, and products

---
+++ app/models/categories.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
