daf03a3c19a48316ec9f09c40da5dc52b423e2c3
mikhail-sereda <mikhail.sereda.dev@yandex.ru>
2026-03-29 14:36:35 +0900

    feat(database.py): create sessionmaker + Base model.

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 # Строка подключения для SQLite
 DATABASE_URL = "sqlite:///ecommerce.db"
 
 # Создаём Engine
 engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):  # New
+    pass
