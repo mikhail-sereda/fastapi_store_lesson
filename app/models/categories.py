de0714912a099a5fa85d04ab5d852eb05bf29cf3
mikhail-sereda <mikhail.sereda.dev@yandex.ru>
2026-04-01 11:37:18 +0900

    feat: add relationship one to many category and product

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Integer, String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -10,3 +10,7 @@ class Category(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    products: Mapped[list["Product"]] = relationship(
+        "Product", back_populates="category"
+    )
--- app/models/products.py
+++ app/models/products.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
-from sqlalchemy import String, Boolean, Integer, Numeric
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, String, Boolean, Integer, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -16,3 +16,14 @@ class Product(Base):
     image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    category_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("categories.id"),
+        nullable=False,
+    )
+
+    category: Mapped["Category"] = relationship(
+        "Category",
+        back_populates="products",
+    )
